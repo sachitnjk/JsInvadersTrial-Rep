4d22ba7caddab3899073aac27624e25e15f518bf
sachit <sachitnjk@gmail.com>
2021-07-31 14:47:58 +0530

    Bounding Box of Canvas

--- Js/script-1.js
+++ Js/script-1.js
@@ -4,12 +4,18 @@ const canvas = document.getElementById("mCanvas");
 const ctx = canvas.getContext("2d");
 
 
-let x = 100;
-let y = 100;
+const player = {
+    x: 100,
+    y: 100,
+    width:50,
+    height:50,
+    color: "WHITE"
+}
 
 let bLeft, bUp, bRight, bDown;
 
 let framesPerSecond = 1000/120;
+
 
 
 /*--------FUNCTIONS--------*/
@@ -19,21 +25,45 @@ function drawRect(x, y, w, h, color){
     ctx.fillRect(x, y, w, h);
 }
 
-function move(){
-    if(bLeft){
-        x--;
+function drawFunc(){
+    drawRect(player.x, player.y, player.width, player.height, player.color);
+}
+
+function collision(player){
+    player.top = player.y;
+    player.bottom = player.y + player.height;
+    player.left = player.x;
+    player.right = player.x + player.width;
+
+    if(player.left < 0){
+        player.x++;
     }
-    if(bUp){
-        y--;
+    if(player.top < 0){
+        player.y++;
     }
-    if(bRight){
-        x++;
+    if(player.right > canvas.width){
+        player.x--;
     }
-    if(bDown){
-        y++;
+    if(player.bottom > canvas.height){
+        player.y--;
     }
 }
 
+function move(){
+    if(bLeft){
+        player.x--;
+    }
+    if(bUp){
+        player.y--;
+    }
+    if(bRight){
+        player.x++;
+    }
+    if(bDown){
+        player.y++;
+    }
+
+}
 
 canvas.addEventListener('keydown', function(e){
     let codes = e.keyCode;
@@ -69,8 +99,13 @@ canvas.addEventListener('keyup', function(e){
 
 
 
-setInterval(function(){
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    move();
-    drawRect(x, y, 50, 50, "#fff");
-}, framesPerSecond);
+function startGame(){
+    setInterval(function(){
+        ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+        move();
+        drawFunc();
+        collision(player);
+    }, framesPerSecond);   
+}
+
+startGame();
